d9cf6c19af8619e05d9a5886e828a7219d10a293
AceyPe <119972705+AceyPe@users.noreply.github.com>
2025-11-30 19:35:18 +0200

    Add files via upload

---
+++ frontend/src/ts/constants/firebase-config-live.ts
@@ -0,0 +1,15 @@
+// To find your config, go to https://console.firebase.google.com/ and select your project
+// Go to (top left) Settings > Project Settings > General
+// scroll down to Your apps > Web Apps (if it doesnt exist, create one) > SDK setup and configuration > select npm
+// your config should be visible there
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA6Dlkw8pPGnVMwSK0Y4TG1A1d2XYKRhiE",
+  authDomain: "monkeytypefork.firebaseapp.com",
+  databaseURL: "https://monkeytypefork-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "monkeytypefork",
+  storageBucket: "monkeytypefork.firebasestorage.app",
+  messagingSenderId: "442121543000",
+  appId: "1:442121543000:web:9e226dc9eae8b0bc96992d",
+};
+
